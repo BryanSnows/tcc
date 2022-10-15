914340152ad2a4cfa3eb5c9a1a104f10f3cf0f19
Bryan Neves <bryan.nevesp@gmail.com>
2022-10-15 12:49:44 -0400

    feat: project sborced

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import './App.css';
-import { Welcome } from './components/Welcome';
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
+
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+import GameOver from "./components/GameOver";
+
+import PickCategory from "./components/PickCategory";
+
+import "./App.css";
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
     <div className="App">
       <h1>Petrobowl Games</h1>
-      <Welcome />
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
+      {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import Quiz from '../img/quiz.svg';
-import './Welcome.css';
 import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 
-export const Welcome = () => {
+import "./Welcome.css";
 
+import Quiz from "../img/quiz.svg";
+
+const Welcome = () => {
   const [quizState, dispatch] = useContext(QuizContext);
 
   return (
-    <div id='welcome'>
-        <h1>Seja bem-vindo</h1>
-        <p>Clique no botão abaixo para começar:</p>
-          <button onClick={() => dispatch({ type: "CHANGE_STAGE" })}>
-             Iniciar
-          </button>
-        <img src={Quiz} alt="Inicio do Game" />
+    <div id="welcome">
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button onClick={() => dispatch({ type: "CHANGE_STAGE" })}>
+        Iniciar
+      </button>
+      <img src={Quiz} alt="Início do Quiz" />
     </div>
-  )
-}
+  );
+};
+
+export default Welcome;
